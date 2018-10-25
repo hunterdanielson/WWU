9e1a137409ad18182533e9ae489484277b7481bf
hunterdanielson <34114734+hunterdanielson@users.noreply.github.com>
2018-10-24 19:37:38 -0700

    working from home

--- CPTR 220 - Web App Dev/student220-master/student220-master/public/checkpoints/cpt16/table_menu.php
+++
@@ -1,9 +0,0 @@
-    <!-- Menu of Tables -->
-    <nav style="text-align: center">
-        <a href="table_1.php">x1</a> | 
-        <a href="table_2.php">x2</a> | 
-        <a href="table_3.php">x3</a> | 
-        <a href="table_4.php">x4</a> | 
-        <a href="table_5.php">x5</a>
-    </nav>
-    <hr>
